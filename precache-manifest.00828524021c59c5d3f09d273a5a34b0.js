f3e306e9532a2d64f5821849355aae95782ae83f
alagappan17 <alagappan1703@gmail.com>
2023-07-24 12:30:21 +0530

    Updates

---
+++ precache-manifest.00828524021c59c5d3f09d273a5a34b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a22720953755872688c6",
+    "url": "/portfolio/static/css/main.0eb52a07.chunk.css"
+  },
+  {
+    "revision": "a22720953755872688c6",
+    "url": "/portfolio/static/js/main.3b732298.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "166fad8303d780324708",
+    "url": "/portfolio/static/js/2.b2db394e.chunk.js"
+  },
+  {
+    "revision": "06f0dfb3dd526cd200d2e0df79f17ce6",
+    "url": "/portfolio/static/media/LouisGeorgeCafeLight.06f0dfb3.ttf"
+  },
+  {
+    "revision": "877dd203e9f65b39ac117010d1cf2a62",
+    "url": "/portfolio/static/media/LouisGeorgeCafe.877dd203.ttf"
+  },
+  {
+    "revision": "98d08bdedfa5004ed5e27ad38fc464b8",
+    "url": "/portfolio/index.html"
+  }
+];
